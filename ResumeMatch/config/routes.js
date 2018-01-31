9b882d4647196f53347909dd410f7c754f7a48da
annineedscoffee <char.anne.spencer@gmail.com>
2018-01-30 20:07:11 -0700

    employer sign up done

--- ResumeMatch/config/routes.js
+++ ResumeMatch/config/routes.js
@@ -12,5 +12,6 @@ module.exports = function(app){
     app.get('/JobSearch', index.js);
     app.get('/SignUp', index.sign);
     app.post('/NewWorker', index.create);
+    app.post('/NewEmployer', index.newemp);
     app.get('/WorkerSearch', index.ws);
 }
